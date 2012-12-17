0068090a476bd3764fc846b620ef03d3a770db45
Sander de Vries <sanderenco@gmail.com>
2012-12-17 16:13:01 +0100

    comparator game

--- js/app/collections/gameCollection.js
+++ js/app/collections/gameCollection.js
@@ -2,10 +2,18 @@
 App.Collections.Game = Backbone.Collection.extend({
     // Specifiy model for this collection
   model: App.Models.Set,
+<<<<<<< HEAD
   comparator: function (game) {
     return game.get('team1Score');
   },
 
+=======
+
+  // Sort on team1Score
+  comparator: function(game){
+    return game.get("team1Score");
+  },
+>>>>>>> comparator game
   // Initialize collection *(backbone method)*
   initialize: function () {
     this.logMessage("Game collection initialized");

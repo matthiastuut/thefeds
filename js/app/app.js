2d05c16df63e1e9457311754e04d93fb5f52137f
ootes <mark.ootes@gmail.com>
2013-01-28 16:14:05 +0100

    updates app.js

--- js/app/app.js
+++ js/app/app.js
@@ -4,32 +4,8 @@
  */
  (function () {
   "use strict";
-    var App = {
-      Models: {},
-      Collections: {},
-      Views: {},
-      Router: {},
-      Data: {}
-    };
 
-    // Define config settings
-    var gameConfig = {
-      tournamentID: 18590,
-      access_token: '109f8a19ae',
-      api_url: 'https://api.leaguevine.com/v1/games/',
-      season_id: '20126'
-    }
-
-    console.log(gameConfig);
-
-    App.Data.Tournament = {
-      tournamentID: 18590,
-      access_token: '109f8a19ae',
-      api_url: 'https://api.leaguevine.com/v1/tournaments/',
-      season_id: '20126'
-    }
-    
-    define([
+   define([
       'app/router/router' // Request router.js
     ], function (Router) {
       var initialize = function () {
